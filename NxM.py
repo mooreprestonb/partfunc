91d9bb7f5f9e576b4772c86a63442eea2aee90d9
mooreprestonb <pmoore@sju.edu>
2026-04-23 10:14:56 -0400

    added timing

--- NxM.py
+++ NxM.py
@@ -6,12 +6,13 @@ import math
 import time
 
 Nx = 4
-Ny = 8
+Ny = 6
 L = Nx*Ny
 Na=8
 comb = math.comb(L,Na)
-print(f"{Nx}x{Ny}={L} with {Na} type A and {comb} combination")
+print(f"{Nx}x{Ny}={L} with {Na} type A and {comb} combinations")
 ngbr = numpy.zeros((L,4),dtype=int)
+ttime = time.perf_counter()
 
 def print_dict(data):
     sum =0
@@ -42,12 +43,12 @@ def generate_grid(N):
 
 #create ngbr array (Nx4) as there are 4 neighbors
 for n in range(L): # periodic neighbors
-    i = n%Nx # i value
-    j = int(n/Nx) # J value
-    nl = (i+1)%Nx+j*Nx # left
-    nr = (i-1)%Nx+j*Nx # right
-    nu = ((j+1)%Ny)*Nx+i # up 
-    nd = ((j-1)%Ny)*Nx+i # down
+    i = n%Nx # i value mod Nx
+    j = int(n/Nx) # J value floor n/Nx
+    nl = (i+1)%Nx + j*Nx # left 
+    nr = (i-1)%Nx + j*Nx # right
+    nu = i        + ((j+1)%Ny)*Nx # up 
+    nd = i        + ((j-1)%Ny)*Nx # down
     ngbr[n]=numpy.array([nl,nr,nu,nd]) # store
     #print(n,i,j,nl,nr,nu,nd)
     #print(n,ngbr[n])
@@ -55,9 +56,10 @@ for n in range(L): # periodic neighbors
 #print(ngbr)
 
 sdict = {}
-keys = ['AA','AB','BA','BB']
+keys = ['AA','AB','BA','BB'] # interaction types 
 ngrid = 0
-stime = time.time()
+stime = ntime = time.time()
+treport = 5
 for grid in generate_grid(Na):
     ngrid += 1
     #print(grid)
@@ -72,11 +74,13 @@ for grid in generate_grid(Na):
     skey = "AA"+str(nb["AA"])+",AB"+str(nb["AB"]+nb["BA"])+",BB"+str(nb["BB"])
     sdict[skey]=sdict.get(skey,0)+1
 
-    if(time.time()-stime>5):
-        print(ngrid,"/",comb,"=",ngrid/comb)
-        stime = time.time()
+    if(time.time()-ntime>treport):
+        print(f"{ngrid}/{comb} = {ngrid/comb:.3f} ~ {(time.time()-stime)*comb/ngrid:.2f} total seconds")
+        ntime = time.time()
         
 #print(sdict)
 print_dict(sdict)
+ttime = time.perf_counter() - ttime
+print(f"Runtime: {ttime:.2f} seconds")
 
 exit()
