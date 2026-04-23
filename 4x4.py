2803579f6ded55f21752ea31a6e1ab7af6fbb7c7
mooreprestonb <pmoore@sju.edu>
2026-04-22 20:14:29 -0400

    enumerate all states from a 4x4

--- 4x4.py
+++ 4x4.py
@@ -1,10 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 from itertools import combinations
+import numpy
+import math
 
-L = 16
+Nx = 4
+Ny = 4
+L = Nx*Ny
 Na=8
+comb = math.comb(L,Na)
+print(Nx,Ny,L,Na,comb)
+ngbr = numpy.zeros((L,4),dtype=int)
 
+def print_dict(data):
+    sum =0
+    for key, value in data.items():
+        print(f"{key}: {value}")
+        sum += value
+    print("N = ",sum)
+    
 def generate_bitmasks(N):
     for positions in combinations(range(L), N):
         mask = 0
@@ -22,5 +36,41 @@ def generate_grid(N):
             grid[pos]='A'
         yield tuple(grid)
 
+#---------------------------------
+# create dict
+
+#ngbr
+for n in range(L):
+    i = n%Nx
+    j = int(n/Nx)
+    nl = (i+1)%Nx+j*Nx
+    nr = (i-1)%Nx+j*Nx
+    nu = ((j+1)%Ny)*Nx+i
+    nd = ((j-1)%Ny)*Nx+i
+    ngbr[n]=numpy.array([nl,nr,nu,nd])
+    #print(n,i,j,nl,nr,nu,nd)
+    #print(n,ngbr[n])
+
+#print(ngbr)
+
+sdict = {}
+keys = ['AA','AB','BA','BB']
+ngrid = 0
 for grid in generate_grid(Na):
-    print(grid)
+    ngrid += 1
+    #print(grid)
+    #print(ngrid)
+    n = [None]*4
+    nb  = dict.fromkeys(keys,0)
+    for i in range(L):
+        id = grid[i]
+        for j in range(4):
+            key = id+grid[ngbr[i][j]]
+            nb[key] += 1
+    skey = "AA"+str(nb["AA"])+",AB"+str(nb["AB"]+nb["BA"])+",BB"+str(nb["BB"])
+    sdict[skey]=sdict.get(skey,0)+1
+
+#print(sdict)
+print_dict(sdict)
+
+exit()
